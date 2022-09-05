114ced280db70a7af18906a8bc553a6d31d8b4cd
embu08 <a800x800@gmail.com>
2022-09-05 22:22:01 +0300

    deleted all the views and added viewset BreedsModelViewSet that can replace them; added router for viewset BreedsModelViewSet;

--- drf_site/cats/views.py
+++ drf_site/cats/views.py
@@ -1,6 +1,6 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,53 +8,6 @@ from .models import Breeds
 from .serializers import BreedsSerializer
 
 
-class BreedsAPIList(generics.ListCreateAPIView):
+class BreedsModelViewSet(viewsets.ModelViewSet):
     queryset = Breeds.objects.all()
     serializer_class = BreedsSerializer
-
-
-class BreedsAPIUpdate(generics.UpdateAPIView):
-    queryset = Breeds.objects.all()
-    serializer_class = BreedsSerializer
-
-
-class BreedsAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Breeds.objects.all()
-    serializer_class = BreedsSerializer
-
-# class BreedsAPIView(APIView):
-#     def get(self, request):
-#         result = Breeds.objects.all().values()
-#         return Response({'title': list(result)})
-#
-#     def post(self, request):
-#         serializer = BreedsSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'post': serializer.data})
-#
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({"error": "method PUT is not allowed"})
-#         try:
-#             instance = Breeds.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Object does not exists"})
-#         serializer = BreedsSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({"post": serializer.data})
-#
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({"error": "method DELETE is not allowed"})
-#         try:
-#             instance = Breeds.objects.get(pk=pk)
-#         except:
-#             return Response({"error": "Objects does not exists or already deleted"})
-#
-#         instance.delete()
-#
-#         return Response({"post": "deleted object " + str(pk)})
--- drf_site/drf_site/urls.py
+++ drf_site/drf_site/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from cats.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'women', BreedsModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/breedslist/', BreedsAPIList.as_view()),
-    path('api/v1/breedslist/<int:pk>/', BreedsAPIUpdate.as_view()),
-    path('api/v1/breedslist/<int:pk>/detail/', BreedsAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls))
 ]
