131c7224af73bba73bb38a3ee7c07e31d89a2f94
embu08 <a800x800@gmail.com>
2022-09-05 12:31:43 +0300

    \serializer for BreedsAPIView was commented out and manually created get and post methods for BreedsAPIView for better understanding how it works

--- drf_site/cats/views.py
+++ drf_site/cats/views.py
@@ -1,10 +1,26 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Breeds
 from .serializers import BreedsSerializer
 
+class BreedsAPIView(APIView):
+    def get(self, request):
+        result = Breeds.objects.all().values()
+        return Response({'title': list(result)})
 
-class BreedsAPIView(generics.ListAPIView):
-    queryset = Breeds.objects.all()
-    serializer_class = BreedsSerializer
+    def post(self, request):
+        post_new = Breeds.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+
+# class BreedsAPIView(generics.ListAPIView):
+#     queryset = Breeds.objects.all()
+#     serializer_class = BreedsSerializer
